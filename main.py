d6e6fb9118d48fa6be564b7e22fd0746bf97b427
Arnav Choudhury <arnav13@gmail.com>
2020-04-12 10:52:31 +0530

    Added Area and Perimeter methods

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
 # Implement a class Rectangle with getter and setter methods and also implement
 # class instance methods like
-	# area,
-	# perimeter,
-	# initialisatoin,
-	# getter,
-	# setter,
+
 	# less than or equal to methods as well.
 
 
 class Rectangle:
-
-	@property
-	def width(self):
-		""" Gets the width"""
-		return self._width
-
-	@property
-	def height(self):
-		""" Gets the height"""
-		return self._height
-
 
 	def __init__(self, height, width):
 		""" Initialises an instance of the Rectangle Class"""
@@ -34,6 +19,15 @@ class Rectangle:
 		self.width = width
 		self.height = height
 
+	@property
+	def width(self):
+		""" Gets the width"""
+		return self._width
+
+	@property
+	def height(self):
+		""" Gets the height"""
+		return self._height
 
 	@width.setter
 	def width(self, width):
@@ -42,10 +36,20 @@ class Rectangle:
 			raise ValueError("Width should be Positive")
 		self._width = width
 
-
 	@height.setter
 	def height(self, height):
 		""" Sets the Height"""
 		if height <= 0:
 			raise ValueError("Height should be Positive")
 		self._height = height
+
+	def area(self):
+		""" Returns the Area of the Rectangle"""
+		return self.width * self.height
+
+	def perimeter(self):
+		""" Returns the Perimeter of the Rectangle"""
+		return 2 * (self.width + self.height)
+	#
+	# def __le__(self, other):
+	# 	""" Implements the Less than or Equal to Operator"""
