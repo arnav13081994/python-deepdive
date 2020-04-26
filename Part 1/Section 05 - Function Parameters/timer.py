d2f5446e966043ef3749db08fc549d76a88958af
Arnav Choudhury <arnav13@gmail.com>
2020-04-26 13:00:54 +0530

    Improved solution

--- Part 1/Section 05 - Function Parameters/timer.py
+++ Part 1/Section 05 - Function Parameters/timer.py
@@ -1,41 +1,40 @@
-
-
-
 def time_it(fn, *args, **kwargs):
 	""" Returns the time taken to run the function"""
-
+	import time
 	t0 = time.time()
 
 	# Execute the function
 	fn(*args, **kwargs)
 
 	t1 = time.time()
-	time = t1-t0
+	time = t1 - t0
 
 	return time
-
-
-
 
 
 # Question: https://www.pythonmorsels.com/exercises/cb8fbdd52cf14f8cb31df4f06343cccf/
 
 
-
-def add(*matrices):
+def add(*matrices_tuple):
 	""" Given n matrices of the same size this function will do an elementwise addition
 	 and return the nex matrix
 	 """
 	from functools import reduce
 
-	# Check if all matrices have the same shape, if not return ValueError
-	matrix_len_lst = [all([len(matrix[0]) == len(element) for element in matrix]) for matrix in matrices]
-	# have the same number of elements
+	# We not only need to check that the 2 lists have the same number of matrices but also that every matrix has the same
+	# number of elements at the corresponding locations in the list of lists passed
+	matrix_len_lst = [
 
-	matrix_len_lst2 = [len(matrices[0]) == len(matrix) for matrix in matrices]
+		all([
+			(len(matrix_of_matrix[0]) == len(element) and len(matrices_tuple[0]) == len(matrix_of_matrix))
+			for element in matrix_of_matrix
+		])
 
-	if all(matrix_len_lst) and all(matrix_len_lst2):
-		return reduce(add_ind_mat, matrices)
+		for matrix_of_matrix in matrices_tuple
+	]
+
+	if all(matrix_len_lst):
+		return reduce(add_ind_mat, matrices_tuple)
 	raise ValueError("Given matrices are not the same size.")
 
 
@@ -43,26 +42,16 @@ def add_ind_mat(mat1, mat2):
 	""" Takes 2 matrices and returns a matrix obtained by doing elementwise addition."""
 
 	tup_iterator = zip(mat1, mat2)
+
 	lst2 = []
 	for i in tup_iterator:
-		unrolled_mat = zip(*i)
+		# Unzip so that you get the new matrix that have corresponding elements together!
+		unzipped_mat = zip(*i)
+
 		lst = []
-		for matrix in unrolled_mat:
+		for matrix in unzipped_mat:
 			lst.append(sum(matrix))
 
 		lst2.append(lst)
 
 	return lst2
-
-
-
-
-
-
-
-
-
-
-
-
-
