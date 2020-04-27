3e8ecc292f6976794fd49a72b21358ef4670011e
Arnav Choudhury <arnav13@gmail.com>
2020-04-27 11:45:28 +0530

    Came up with solution#2 for the problem that is super effecient. Uses a nested list comprehensoin and early terminates calculation if even a matrix is of a different size in the list of matrices

--- python_problems/problem1.py
+++ python_problems/problem1.py
@@ -4,6 +4,11 @@ Question: https://www.pythonmorsels.com/exercises/cb8fbdd52cf14f8cb31df4f06343cc
 """
 
 # Solution #1
+
+def raise_ValError():
+	raise ValueError("Given matrices are not the same size.")
+
+
 def add(*matrices_tuple):
 	""" Given n matrices of the same size this function will do an elementwise addition
 	 and return the nex matrix
@@ -12,10 +17,14 @@ def add(*matrices_tuple):
 
 	# We not only need to check that the 2 lists have the same number of matrices but also that every matrix has the same
 	# number of elements at the corresponding locations in the list of lists passed
+
 	matrix_len_lst = [
 
 		all([
-			(len(matrix_of_matrix[0]) == len(element) and len(matrices_tuple[0]) == len(matrix_of_matrix))
+			(
+					len(matrix_of_matrix[0]) == len(element)
+					and len(matrices_tuple[0]) == len(matrix_of_matrix) or raise_ValError()
+			)
 			for element in matrix_of_matrix
 		])
 
@@ -23,22 +32,19 @@ def add(*matrices_tuple):
 	]
 
 	if all(matrix_len_lst):
-		return reduce(add_ind_mat, matrices_tuple)
-	raise ValueError("Given matrices are not the same size.")
+		return reduce(add_ind_mat_v1, matrices_tuple)
+	raise_ValError()
 
 
-def add_ind_mat(mat1, mat2):
+def add_ind_mat_v1(mat1, mat2):
 	""" Takes 2 matrices and returns a matrix obtained by doing elementwise addition."""
-
-	tup_iterator = zip(mat1, mat2)
-
 	lst2 = []
-	for i in tup_iterator:
-		# Unzip so that you get the new matrix that have corresponding elements together!
-		unzipped_mat = zip(*i)
 
+	for i in zip(mat1, mat2):
+		# Unzip so that you get the new matrix that have corresponding elements together!
 		lst = []
-		for matrix in unzipped_mat:
+
+		for matrix in zip(*i):
 			lst.append(sum(matrix))
 
 		lst2.append(lst)
@@ -49,8 +55,17 @@ def add_ind_mat(mat1, mat2):
 # Solution #2
 
 
+def add_ind_mat_v2(mat1, mat2):
+	""" Takes 2 matrices and returns a matrix obtained by doing elementwise addition."""
 
+	lst = [
+		[
+			k+j for k, j in zip(*i)
+		]
+		for i in zip(mat1, mat2)
+	]
 
+	return lst
 
 
 # Solution #3
