0bfdd32f51b318bcc9963953ced35c8952ad5d30
Arnav Choudhury <arnav13@gmail.com>
2020-04-29 19:51:28 +0530

    Made code slightly more effecient

--- python_problems/exercises/lists.py
+++ python_problems/exercises/lists.py
@@ -9,7 +9,7 @@ def get_vowel_names(names):
 
 def power_list(lst):
     """Return a list that contains each number raised to the i-th power."""
-    return [num**index for index, num in enumerate(lst)]
+    return [num ** index for index, num in enumerate(lst)]
 
 
 def flatten(a):
@@ -19,7 +19,8 @@ def flatten(a):
 
 def reverse_difference(lst):
     """Return list subtracted from the reverse of itself."""
-    return [a-b for a, b in zip(lst, lst[::-1])]
+    return [a - b for a, b in zip(lst, lst[::-1])]
+
 
 def matrix_add(m1, m2):
     """Add corresponding numbers in given 2-D matrices."""
@@ -36,21 +37,21 @@ def transpose(matrix):
 
 def get_factors(n):
     """Return a list of all factors of the given number."""
-    return [number for number in range(1, n+1) if not n % number]
+    return [number for number in range(1, n + 1) if not n % number]
+
 
 #  TODO Come up with a much better implementation using hashmaps and also make sure the triplets are unique
 def triples(num):
     """Return list of Pythagorean triples less than input num."""
     # lst_seen = []
     ans = []
-    lst = range(1, num)
+    lst = [i ** 2 for i in range(1, num)]
     for i in lst:
         # Now the question is the same thing as saying find 2 numbers from the list
         # S.T the square of their sum == i**2
-        target_sum = i**2
         for j in lst:
             if j != i:
-                to_find = (target_sum - j**2)**0.5
+                to_find = (i - j) ** 0.5
                 try:
                     if to_find == int(to_find):
                         to_find = int(to_find)
@@ -58,6 +59,5 @@ def triples(num):
                             ans.append([i, j, to_find])
                 except Exception as e:
                     continue
-
 
     return ans
