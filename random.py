9ca1d46f5eca0bdcaec1ae71602994198b29d44d
Arnav Choudhury <arnav13@gmail.com>
2020-05-07 17:29:01 +0530

    Very simple decorator class and factory

--- random.py
+++ random.py
@@ -1,12 +1,36 @@
 # use fn.__code__.co__freevars: for free variable tuple
 # use fn.__closure__ for the shared cell tuple info.
 
+
+def dec_fact1(a, b):
+	def dec(f):
+		def inner(*args, **kwargs):
+			print("About to run the function {0} and {1}".format(a, b))
+			return f(*args, **kwargs)
+
+		return inner
+
+	return dec
+
+
+def add(a, b):
+	return a + b
+
+
+class dec_fact:
+
+	def __init__(self, a, b):
+		self.a = a
+		self.b = b
+
+	def __call__(self, f):
+		def inner(*args, **kwargs):
+			print("About to run the function {0} and {1}".format(self.a, self.b))
+			return f(*args, **kwargs)
+
+		return inner
+
+
 if __name__ == "__main__":
-	x = ['erferferfe']
-	i1 = outer()
-	# i2 = i1()
-	# i3 = i2()
-	# i4 = i3()
-# print(i1.__closure__)
-# print(i2.__closure__)
-# print(i3.__closure__)
+	add = dec_fact(10, 20)(add)
+	print(add(1, 5))
