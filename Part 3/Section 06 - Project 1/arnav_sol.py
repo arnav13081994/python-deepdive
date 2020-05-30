f576d54f6e769ce3c956fa83bff21bb9f6057c70
Arnav Choudhury <arnav13@gmail.com>
2020-05-30 10:52:18 +0530

    Much better error handling and reporting in the JSON template checking function

--- Part 3/Section 06 - Project 1/arnav_sol.py
+++ Part 3/Section 06 - Project 1/arnav_sol.py
@@ -7,23 +7,91 @@ def validate(data, template, path=[]):
 	for key, value in template.items():
 
 		if key not in data:
-			path += [key]
-			return False, KeyError(f"Key: {'.'.join(path)} is not present")
+			raise KeyError(f"Key: {'.'.join(path + [key])} is not present")
 
-		if not isinstance(value, dict):
-			# This is the case of single data type checking
-			if not isinstance(data[key], value):
-				path += [key]
-				return False, TypeError(
-					f"Expected Type: {value} for key: {'.'.join(path)}. Received: {type(data[key])}")
-		elif isinstance(value, dict):
+		if not isinstance(value, dict) and not isinstance(data[key], value):
+			raise TypeError(
+				f"Expected Type: {value} for key: {'.'.join(path + [key])}. Received: {type(data[key])}")
 
-			path += [key]
-			state, error = validate(data[key], value, path=path)
-			if not state:
-				return state, error
-			else:
-				path.pop()
+		if isinstance(value, dict):
+			validate(data[key], value, path=path + [key])
 
 	return True, ''
 
+
+if __name__ == "__main__":
+	template = {
+		'user_id': int,
+		'name': {
+			'first': str,
+			'last': str
+		},
+		'bio': {
+			'dob': {
+				'year': int,
+				'month': int,
+				'day': int
+			},
+			'birthplace': {
+				'country': str,
+				'city': str
+			}
+		}
+	}
+	john = {
+		'user_id': 100,
+		'name': {
+			'first': 'John',
+			'last': 'Cleese'
+		},
+		'bio': {
+			'dob': {
+				'year': 1939,
+				'month': 11,
+				'day': 27
+			},
+			'birthplace': {
+				'country': 'United Kingdom',
+				'city': 'Weston-super-Mare'
+			}
+		}
+	}
+	michael = {
+		'user_id': 102,
+		'name': {
+			'first': 'Michael',
+			'last': 'Palin'
+		},
+		'bio': {
+			'dob': {
+				'year': 1943,
+				'month': 'May',
+				'day': 5
+			},
+			'birthplace': {
+				'country': 'United Kingdom',
+				'city': 'Sheffield'
+			}
+		}
+	}
+	eric = {
+		'user_id': 101,
+		'name': {
+			'first': 'Eric',
+			'last': 'Idle'
+		},
+		'bio': {
+			'dob': {
+				'year': 1943,
+				'month': 3,
+				'day': 29
+			},
+			'birthplace': {
+				'country': 'United Kingdom'
+			}
+		}
+	}
+
+	print(validate(john, template))
+	print(validate(eric, template))
+	print(validate(michael, template))
