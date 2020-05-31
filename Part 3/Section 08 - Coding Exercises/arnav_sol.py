56d73df61a2c4d6d1ed06b960bc4a65827eb0801
Arnav Choudhury <arnav13@gmail.com>
2020-05-31 19:06:53 +0530

    Created a cusomt JSOn encoder to jsonify 2 cusomt classes as well as Date, Datetime, and Decimal classes that will also all a customDecoder to deserialsie data into the correct format right off the bat in python

---
+++ Part 3/Section 08 - Coding Exercises/arnav_sol.py
@@ -0,0 +1,118 @@
+from datetime import date, datetime
+from decimal import Decimal
+from json import JSONEncoder, dumps
+from functools import singledispatch
+
+
+class Stock:
+	def __init__(self, symbol, date, open_, high, low, close, volume):
+		self.symbol = symbol
+		self.date = date
+		self.open = open_
+		self.high = high
+		self.low = low
+		self.close = close
+		self.volume = volume
+
+	def __repr__(self):
+		return f'Stock(symbol={self.symbol}, date={self.date}, open={self.open}, high={self.high},' \
+		       f' low={self.low}, close={self.close},  volume={self.volume})'
+
+	def toJSON(self):
+		"""
+		This class method will return a dictionary that will let the deserializer know the objecttype as well
+		"""
+		return {
+			'objectType': 'Stock',
+			'properties': {
+				'symbol': self.symbol,
+				'date': self.date,
+				'open': self.open,
+				'high': self.high,
+				'low': self.low,
+				'close': self.close,
+				'volume': self.volume
+			}
+		}
+
+
+class Trade:
+	def __init__(self, symbol, timestamp, order, price, volume, commission):
+		self.symbol = symbol
+		self.timestamp = timestamp
+		self.order = order
+		self.price = price
+		self.commission = commission
+		self.volume = volume
+
+	def __repr__(self):
+		return f'Trade(symbol={self.symbol}, timestamp={self.timestamp}, order={self.order}, price={self.price},' \
+		       f' commission={self.commission}, volume={self.volume})'
+
+	def toJSON(self):
+		"""
+		This class method will return a dictionary that will let the deserializer know the objecttype as well
+		"""
+		return {
+			'objectType': 'Trade',
+			'properties': {
+				'symbol': self.symbol,
+				'timestamp': self.timestamp,
+				'order': self.order,
+				'price': self.price,
+				'commission': self.commission,
+				'volume': self.volume
+			}
+		}
+
+
+class CustomJSONEncoder(JSONEncoder):
+
+	def __init__(self, *args, **kwargs):
+
+		my_params = {
+			"indent": 2,
+		}
+
+		super().__init__(**{**kwargs, **my_params})
+
+
+	#  TODO In python 3.8 you can use singledispatchemthod which lets you decorate class instace methods
+
+	def default(self, obj):
+		''' The encoder will call this functoin when it emncounters something it doesn't know how to serialise.'''
+
+		try:
+			if isinstance(obj, Stock) or isinstance(obj, Trade):
+				return obj.toJSON()
+			elif isinstance(obj, Decimal):
+				return f'Decimal({str(obj)})'
+			elif isinstance(obj, date) or isinstance(obj, datetime):
+				return obj.isoformat()
+		except TypeError:
+			super().default(obj)
+
+
+
+if __name__ == "__main__":
+	activity = {
+		"quotes": [
+			Stock('TSLA', date(2018, 11, 22), Decimal('338.19'), Decimal('338.64'), Decimal('337.60'),
+			      Decimal('338.19'),
+			      365_607),
+			Stock('AAPL', date(2018, 11, 22), Decimal('176.66'), Decimal('177.25'), Decimal('176.64'),
+			      Decimal('176.78'),
+			      3_699_184),
+			Stock('MSFT', date(2018, 11, 22), Decimal('103.25'), Decimal('103.48'), Decimal('103.07'),
+			      Decimal('103.11'),
+			      4_493_689)
+		],
+
+		"trades": [
+			Trade('TSLA', datetime(2018, 11, 22, 10, 5, 12), 'buy', Decimal('338.25'), 100, Decimal('9.99')),
+			Trade('AAPL', datetime(2018, 11, 22, 10, 30, 5), 'sell', Decimal('177.01'), 20, Decimal('9.99'))
+		]
+	}
+
+	dup_1 = dumps(activity, cls=CustomJSONEncoder)
+	print(dup_1)
