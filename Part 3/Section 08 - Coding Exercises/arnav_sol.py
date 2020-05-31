bea14dd8dc36b8417a612d1d6f1a586d6d6f0179
Arnav Choudhury <arnav13@gmail.com>
2020-05-31 20:28:38 +0530

    Working on customdecoder

--- Part 3/Section 08 - Coding Exercises/arnav_sol.py
+++ Part 3/Section 08 - Coding Exercises/arnav_sol.py
@@ -17,22 +17,46 @@ class Stock:
 		return f'Stock(symbol={self.symbol}, date={self.date}, open={self.open}, high={self.high},' \
 		       f' low={self.low}, close={self.close},  volume={self.volume})'
 
+
 	def toJSON(self):
 		"""
 		This class method will return a dictionary that will let the deserializer know the objecttype as well
 		"""
 		return {
-			'objecttype': 'Stock',
+			'objecttype': 'stock',
 			'properties': {
-				'symbol': self.symbol,
-				'date': self.date,
-				'open': self.open,
-				'high': self.high,
-				'low': self.low,
-				'close': self.close,
-				'volume': self.volume
+				'symbol': {
+					'objecttype': 'string',
+					'value': self.symbol,
+				},
+				'date': {
+					'objecttype': 'date',
+					'value': self.date,
+				},
+				'open': {
+					'objecttype': 'decimal',
+					'value': self.open,
+				},
+				'high': {
+					'objecttype': 'decimal',
+					'value': self.high,
+				},
+				'low': {
+					'objecttype': 'decimal',
+					'value': self.low,
+				},
+				'close': {
+					'objecttype': 'decimal',
+					'value': self.close,
+				},
+				'volume': {
+					'objecttype': 'integer',
+					'value': self.volume,
+				},
 			}
 		}
+
+
 
 
 class Trade:
@@ -53,14 +77,32 @@ class Trade:
 		This class method will return a dictionary that will let the deserializer know the objecttype as well
 		"""
 		return {
-			'objecttype': 'Trade',
+			'objecttype': 'trade',
 			'properties': {
-				'symbol': self.symbol,
-				'timestamp': self.timestamp,
-				'order': self.order,
-				'price': self.price,
-				'commission': self.commission,
-				'volume': self.volume
+				'symbol': {
+					'objecttype': 'string',
+					'value': self.symbol,
+				},
+				'timestamp': {
+					'objecttype': 'datetime',
+					'value': self.timestamp,
+				},
+				'order': {
+					'objecttype': 'string',
+					'value': self.order,
+				},
+				'price': {
+					'objecttype': 'decimal',
+					'value': self.price,
+				},
+				'commission': {
+					'objecttype': 'decimal',
+					'value': self.commission,
+				},
+				'volume': {
+					'objecttype': 'integer',
+					'value': self.volume,
+				},
 			}
 		}
 
@@ -73,26 +115,59 @@ class CustomJSONEncoder(JSONEncoder):
 			"indent": 2,
 		}
 
-		# By default, overriding any arguements mentioned here regardless of what the dev puts to maintain consistency
-		super().__init__(*args, **{**kwargs, **my_params})
-
+		# By default, overriding any arguments mentioned here regardless of what the dev puts to maintain consistency
+		super().__init__(**{**kwargs, **my_params})
 
 	#  TODO In python 3.8 you can use singledispatchemthod which lets you decorate class instace methods
 
 	def default(self, obj):
-		''' The encoder will call this functoin when it emncounters something it doesn't know how to serialise.'''
-
+		''' The encoder will call this function when it emcounters something it doesn't know how to serialise.'''
 		try:
 			if isinstance(obj, Stock) or isinstance(obj, Trade):
 				return obj.toJSON()
 			elif isinstance(obj, Decimal):
-				return f'Decimal({str(obj)})'
+				return float(obj)
 			elif isinstance(obj, date) or isinstance(obj, datetime):
 				return obj.isoformat()
-
 		#  Best to call the parents default so that correct error handling and reporting functionality can be tapped into.
 		except TypeError:
 			super().default(obj)
+
+
+class CustomJSONDecoder(JSONDecoder):
+
+	def __init__(self, *args, **kwargs):
+		my_params = {
+			"object_hook": self.deal_with_obj_hook,
+			'parse_float': self.deal_with_parse_float,
+		}
+
+		# By default, overriding any arguments mentioned here regardless of what the dev puts to maintain consistency
+		super().__init__(**{**kwargs, **my_params})
+
+
+
+	def deal_with_parse_float(self, obj):
+		return Decimal(obj)
+
+	def deal_with_obj_hook(self, obj):
+		''' This function will get all the dict objects root or nested and will need to return a dict object'''
+
+		for key, value in obj.items():
+			if (key, value) == ('objecttype', 'decimal'):
+				return obj['value']
+			elif (key, value) == ('objecttype', 'integer'):
+				return obj['value']
+			elif (key, value) == ('objecttype', 'string'):
+				return obj['value']
+			elif (key, value) == ('objecttype', 'date'):
+				return date.fromisoformat(obj['value'])
+			elif (key, value) == ('objecttype', 'datetime'):
+				return datetime.fromisoformat(obj['value'])
+			elif key == 'properties':
+				return obj['properties']
+
+		return obj
 
 
 
@@ -100,14 +175,11 @@ if __name__ == "__main__":
 	activity = {
 		"quotes": [
 			Stock('TSLA', date(2018, 11, 22), Decimal('338.19'), Decimal('338.64'), Decimal('337.60'),
-			      Decimal('338.19'),
-			      365_607),
+			      Decimal('338.19'), 365_607),
 			Stock('AAPL', date(2018, 11, 22), Decimal('176.66'), Decimal('177.25'), Decimal('176.64'),
-			      Decimal('176.78'),
-			      3_699_184),
+			      Decimal('176.78'), 3_699_184),
 			Stock('MSFT', date(2018, 11, 22), Decimal('103.25'), Decimal('103.48'), Decimal('103.07'),
-			      Decimal('103.11'),
-			      4_493_689)
+			      Decimal('103.11'), 4_493_689)
 		],
 
 		"trades": [
@@ -116,5 +188,8 @@ if __name__ == "__main__":
 		]
 	}
 
-	dup_1 = dumps(activity, cls=CustomJSONEncoder, indent=10)
-	print(dup_1)
+	serialise = dumps(activity, cls=CustomJSONEncoder, indent=10)
+	# print(serialise)
+
+	deserialise = loads(serialise, cls=CustomJSONDecoder)
+	print(deserialise)
