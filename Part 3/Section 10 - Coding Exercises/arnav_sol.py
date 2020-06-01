f524ecc0bbe587b448d2ccb3aa0a2b1360582ff1
Arnav Choudhury <arnav13@gmail.com>
2020-06-01 11:00:57 +0530

    Came up with a memory effecient solution for merging n number of dictoinarues and returning word frequencies.

--- Part 3/Section 10 - Coding Exercises/arnav_sol.py
+++ Part 3/Section 10 - Coding Exercises/arnav_sol.py
@@ -1,6 +1,7 @@
 from collections import defaultdict, Counter
 from itertools import chain, repeat
 
+
 # Using Default dict
 def merge_defdict(*dicts):
 	'''
@@ -15,6 +16,7 @@ def merge_defdict(*dicts):
 	sorted_dic = dict(sorted(sorted_dic.items(), key=lambda x: x[1], reverse=True))
 	return sorted_dic
 
+
 # Using Counter object
 def merge_counter_v1(*dicts):
 	'''
@@ -27,6 +29,14 @@ def merge_counter_v1(*dicts):
 	return dict(c.most_common())
 
 
+# Using Counter and chain and repeat
+def merge_counter_v2(*dicts):
+	'''
+	Merges all passed in dicts and increases the frequency of keys and returns the sorted version back
+	'''
+	p = (repeat(*item) for dic in dicts for item in dic.items())
+	return dict(Counter(chain.from_iterable(p)).most_common())
+
 
 if __name__ == "__main__":
 	d1 = {'python': 10, 'java': 3, 'c#': 8, 'javascript': 15}
@@ -34,4 +44,4 @@ if __name__ == "__main__":
 	d3 = {'erlang': 5, 'haskell': 2, 'python': 1, 'pascal': 1}
 	print(merge_defdict(d1, d2, d3))
 	print(merge_counter_v1(d1, d2, d3))
-
+	print(merge_counter_v2(d1, d2, d3))
