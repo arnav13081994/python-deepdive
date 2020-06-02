26fc2b0698eacc5533b136898ab3cf369b83a0c0
Arnav Choudhury <arnav13@gmail.com>
2020-06-02 21:10:09 +0530

    Added validators for validating the name and offsets for hour and minutes

--- Part 4/Section 03 - Project 1/arnav_sol.py
+++ Part 4/Section 03 - Project 1/arnav_sol.py
@@ -37,10 +37,13 @@ from datetime import timedelta
 class TimeZone:
 
 	def __init__(self, name, offset_hr, offset_min):
-		print("Initialsing")
-		self._name = name
-		self._offset_hr = offset_hr
-		self._offset_min = offset_min
+
+		print("Validating and  Initialsing")
+		self._name = self.validate_name(name)
+		self._offset_hr = self.validate_offset_hr(offset_hr)
+		self._offset_min = self.validate_offset_min(offset_min)
+
+
 		self._offset = None
 
 	# TODO At class instance init time, setters and geters are not called. They are called when the class instance attributes are requeted or updated after clreation and initilsaition only.
@@ -56,11 +59,8 @@ class TimeZone:
 		""" Valid timezone name can only contain letters, has no leading or trailing spaces and is of type str"""
 		print("running name setter....")
 
-		if not isinstance(value, str):
-			raise TypeError("Timezone must be of type str")
-		elif not value.strip() or not value.strip().isalpha():
-			raise ValueError("Timezone name cannot be empty or contain anything but letters.")
-		self._name = value.strip()
+		# validate and set the name
+		self._name = self.validate_name(value)
 
 	@property
 	def offset_hr(self):
@@ -69,17 +69,12 @@ class TimeZone:
 
 	@offset_hr.setter
 	def offset_hr(self, value):
-		# Validate tz offset hour
-
 		print("settings offset hour....")
-		if not isinstance(value, Integral):
-			raise TypeError("Hours Offset needs to be an integer")
-		elif not (-12 <= value <= 14):
-			raise ValueError("Hours Offset must be between -12 and +14")
-		self._offset_hr = value
+		# Validate and set tz offset hour
+		self._offset_hr = self.validate_offset_hr(value)
 
-		# Trigger update for _offset
-		self._offset = timedelta(hours=self._offset_hr)
+	# Trigger update for _offset
+	# self._offset = timedelta(hours=self._offset_hr)
 
 	@property
 	def offset_min(self):
@@ -88,17 +83,13 @@ class TimeZone:
 
 	@offset_min.setter
 	def offset_min(self, value):
-		# Validate the TZ offset_min
+		# Validate and set the TZ offset_min
 		print("settings offset minute....")
-		if not isinstance(value, Integral):
-			raise TypeError("Minutes Offset needs to be an integer")
-		elif not (0 <= value <= 59):
-			raise ValueError("Minutes Offset must be between 0 and +59. Sign of hours would be used to create"
-			                 " the correct offset")
-		self._offset_min = value
 
-		# Trigger update for _offset
-		self._offset = timedelta(minutes=self._offset_min)
+		self._offset_min = self.validate_offset_min(value)
+
+	# Trigger update for _offset
+	# self._offset = timedelta(minutes=self._offset_min)
 
 	@property
 	def offset(self):
@@ -113,20 +104,51 @@ class TimeZone:
 			self._offset = value
 
 	def __eq__(self, other):
-		return (isinstance(other, TimeZone) and
-		        self._name == other._name and
-		        self._offset_hr == other._offset_hr and
-		        self._offset_min == other._offset_min)
+		return (
+				isinstance(other, TimeZone) and
+				self._name == other._name and
+				self._offset_hr == other._offset_hr and
+				self._offset_min == other._offset_min
+		)
 
 	def __repr__(self):
 		return f'TimeZone(name={self._name}, offset_hr={self._offset_hr}, offset_min={self._offset_min})'
+
+
+
+
+	# Validators Functions
+	@staticmethod
+	def validate_offset_min(value):
+		if not isinstance(value, Integral):
+			raise TypeError("Minutes Offset needs to be an integer")
+		elif not (0 <= value <= 59):
+			raise ValueError(
+				"Minutes Offset must be between 0 and +59. Sign of hours would be used to create the correct offset"
+			)
+		return value
+
+	@staticmethod
+	def validate_offset_hr(value):
+		if not isinstance(value, Integral):
+			raise TypeError("Hours Offset needs to be an integer")
+		elif not (-12 <= value <= 14):
+			raise ValueError("Hours Offset must be between -12 and +14")
+		return value
+
+	@staticmethod
+	def validate_name(value):
+		if not isinstance(value, str):
+			raise TypeError("Timezone must be of type str")
+		elif not value.strip() or not value.strip().isalpha():
+			raise ValueError("Timezone name cannot be empty or contain anything but letters.")
+		return value.strip()
 
 
 if __name__ == "__main__":
 	tz1 = TimeZone('ABC', -2, -15)
 	tz2 = TimeZone('ABC', -2, -15)
 	print(tz1)
-	print(tz1 == tz2)
-	tz1.offset_hr = 10
-	tz1.offset_min = 100
+	tz1.offset_hr = -2
+	tz1.offset_min = -15
 	print(tz1)
