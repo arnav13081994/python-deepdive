737544c1c5c0f81b7b5bf5ea416917034a5f29c8
Arnav Choudhury <arnav13@gmail.com>
2020-06-03 18:56:03 +0530

    Documenting a few key test cases for the Mod Class.

--- Part_4/Section05_Project2/test_arnav_sol.py
+++ Part_4/Section05_Project2/test_arnav_sol.py
@@ -1,19 +1,20 @@
 '''
-Write tests for the following cases:
+Test Cases:
 
-1) value is positive integral
-1) value is negative integral
-2) Modulus is positive integral
-3) Value is positive but not integral
-3) Value is negative but not integral
-4) Modulus is positive but not integral
-5) Modulus is negative integral
-6) Modulus is negative but not integral
-7) Attribute error for trying ot set either the value or the modulus of a MOD object instance
+1) Expected Exceptions for invalid Mod.value and Mod.modulus attrs.
+2) Expected Exceptions for trying to set read-only Mod.value and Mod.modulus attrs.
+3) Expected Behaviour for valid Mod.value and Mod.modulus attrs.
+4)
+
 '''
+
+
+
+
+
+
 import pytest
 from Part_4.Section05_Project2.arnav_sol import Mod
-
 
 
 # Helper Function
