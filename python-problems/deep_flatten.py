e1943a7e5a2a96b11df3db70ec239e0fae9a0393
Arnav Choudhury <arnav13@gmail.com>
2020-06-09 15:40:04 +0530

    Refactoring

--- python-problems/deep_flatten.py
+++ python-problems/deep_flatten.py
@@ -1,25 +1,20 @@
-def deep_flatten(matrix_lst):
-	"""
-	Will take in a list of iterables and return a flattened iterable
-	"""
-	from collections.abc import Iterable
-	for matrix in matrix_lst:
+from collections.abc import Iterable
 
+def deep_flatten(matrix_itr):
+	"""
+	Will take in an Iterable of iterables and returns a flattened iterable
+	"""
+	for matrix in matrix_itr:
 		if isinstance(matrix, Iterable) and not isinstance(matrix, str):
 			yield from deep_flatten(matrix)
-
 		else:
 			yield matrix
 
 
 if __name__ == "__main__":
 	lst = [
-		[(1, 2), (3, 4)],
-		[(5, 6), (7, 8)]
+		[(1, 2), [(3, '4', 'arnav')]],
+		[('5', 6), [(7, ['8'])]]
 	]
 
-	ans = deep_flatten(lst)
-	print(ans)
-
-	while True:
-		print(next(ans))
+	print(list(deep_flatten(lst)))
